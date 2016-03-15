c9bdd5c835b24d97180dc9b8102d93b70a57ad01
jciancio <jciancio7@gmail.com>
2016-03-15 17:34:13 -0400

    added some tests

--- spec/features/visiting_homepage_spec.rb
+++ spec/features/visiting_homepage_spec.rb
@@ -11,8 +11,12 @@ feature 'Visting the Homepage' do
     find_field('Email Confirmation')
     find_field('Password')
     find_field('Password Confirmation')
+    find_link('Register')
   end
 
   it 'displays a login form' do
+    find_field('Email')
+    find_field('Password')
+    find_link('Login')
   end
 end
