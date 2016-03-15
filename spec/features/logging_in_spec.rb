20130327ae8c72290615ad02a8b845287d01b68c
jciancio <jciancio7@gmail.com>
2016-03-14 21:38:22 -0400

    initial commit after forking from student-resources repo. Only some pending tests, no models, etc.

---
+++ spec/features/logging_in_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+feature 'Logging In' do
+
+  it 'creates a new session'
+  it 'sets a current user'
+end
---
+++ spec/features/logging_out_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+feature 'Logging Out' do
+  it 'destroys the session'
+  it 'removes the current user'
+end
