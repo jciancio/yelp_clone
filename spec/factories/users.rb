4ed6ffe955960eff24612ef6d7bbcedee012ba8b
jciancio <jciancio7@gmail.com>
2016-03-15 22:15:03 -0400

    fixed factory to use only name

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,7 @@ require 'faker'
 
 FactoryGirl.define do
   factory :user do
-    sequence(:first_name)  { |n| Faker::Name.first_name + n.to_s }
-    sequence(:last_name)   { |n| Faker::Name.last_name + n.to_s }
+    sequence(:name)  { |n| Faker::Name.name + n.to_s }
     password               { Faker::Internet.password(8) }
     sequence(:email)       { |n| n.to_s + Faker::Internet.free_email }
   end
