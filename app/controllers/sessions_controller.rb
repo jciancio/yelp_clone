fe21bb568395d7709ef7152a74867197581b0ec5
jciancio <jciancio7@gmail.com>
2016-03-16 22:51:22 -0400

    fixed controller to redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      user_path(user), notice: 'Loggin In!'
+      redirect_to user_path(user), notice: 'Loggin In!'
     else
       render :new
     end
