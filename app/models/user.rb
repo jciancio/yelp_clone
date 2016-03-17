74e2bb5316b9aa1f77b42117d89282f92a87b96a
jciancio <jciancio7@gmail.com>
2016-03-16 20:31:55 -0400

    added validations and better to_s

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :businesses
   has_one :photo
+
+  validates :name, presence: true
+  validates :email, presence: true
+            uniqueness: true
+
+  def to_s
+    "#{name}"
+  end
 end
