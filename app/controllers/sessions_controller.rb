5a34f7340f06dc55a440f3317403d5f11a593822
jciancio <jciancio7@gmail.com>
2016-03-17 15:50:01 -0400

    updated notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user_path(user), notice: 'Loggin In!'
+      redirect_to user_path(user), notice: 'Logged In!'
     else
       render :new
     end
