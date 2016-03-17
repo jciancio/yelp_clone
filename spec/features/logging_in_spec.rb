eb68575bdf282977cab6468937975c2249d62e30
jciancio <jciancio7@gmail.com>
2016-03-16 23:38:02 -0400

    created test for logging in

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 feature 'Logging In' do
+  before do
+    @user = FactoryGirl.build(:user)
+    @user.email_confirmation = @user.email
+    @user.password_confirmation = @user.password
+    @user.save
+  end
 
-  it 'creates a new session'
+  it 'creates a new session'do
+    visit root_path
+    find_field('user[email]')
+    find_field('user[password]')
+    fill_in 'user[email]', with: @user.email
+    fill_in 'user[password]', with: @user.password
+    click_button('Log In')
+    expect(current_path).to eq(sessions_path)
+    page.has_content? @user.email
+  end
   it 'sets a current user'
 end
