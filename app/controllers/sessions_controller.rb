d2eb943e197dd5845f373d42a9b45d1dfae5b098
jciancio <jciancio7@gmail.com>
2016-03-16 22:44:23 -0400

    added notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      user_path
+      user_path, notice: 'Loggin In!'
     else
       render :new
     end
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path
+    redirect_to root_path, 'Logged Out!'
   end
 end
