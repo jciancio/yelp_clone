44fd8a086763cfef7541535f8ca9acf428325bab
jciancio <jciancio7@gmail.com>
2016-03-17 16:21:30 -0400

    need to revisit my approach to these tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  before do
+    @user = FactoryGirl.build(:user)
+    @user.email_confirmation = @user.email
+    @user.password_confirmation = @user.password
+    @user.save
+  end
 
   describe "GET #new" do
     it "returns http success" do
@@ -9,17 +15,27 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+    it "returns http redirect" do
+      user = @user
+      user.authenticate(@user.password)
+      post :create
+      expect(response).to have_http_status(:redirect)
+    end
+
+    it "adds user id to the session object" do
+      expect(session[:user_id]).to eq(@user.id)
     end
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+  describe "DELETE #destroy" do
+    it "returns http redirect" do
+      delete :destroy, id: @user.id
+      expect(response).to have_http_status(:redirect)
+    end
+
+    it "destroys the session" do
+      expect(session[:user_id]).to eq(nil)
     end
   end
 
--- spec/features/logging_out_spec.rb
+++ spec/features/logging_out_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 feature 'Logging Out' do
-  it 'destroys the session'
+  before do
+    @user = FactoryGirl.build(:user)
+    @user.email_confirmation = @user.email
+    @user.password_confirmation = @user.password
+    @user.save
+  end
+
+  it 'redirects to root path' do
+    visit root_path
+    find_field('user[email]')
+    find_field('user[password]')
+    fill_in 'user[email]', with: @user.email
+    fill_in 'user[password]', with: @user.password
+    click_button('Log In')
+    expect(page).to have_content('Log Out')
+    page.has_content? 'Log Out'
+  end
   it 'removes the current user'
 end
