470db3f02edc3f27dd9b4181f40a9865e239f5a5
jciancio <jciancio7@gmail.com>
2016-03-17 22:18:57 -0400

    set up bus params, set_bus before method, and setting user when creating business, still need to finish controller

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,14 +1,25 @@
 class BusinessesController < ApplicationController
+  before_action :set_business, only: [:show, :edit, :update, :destroy]
+
   def index
+    @businesses = Business.all
   end
 
   def show
   end
 
   def new
+    @business = Business.new
   end
 
   def create
+    @business = current_user.businesses.build(business_params)
+
+    if @business.save
+      redirect_to @business, notice: 'Business added!'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +29,14 @@ class BusinessesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def set_business
+    @business = Business.find(params[:id])
+  end
+
+  def business_params
+    params.require(:business).permit(:name, :description, :address, :city, :state, :zip)
   end
 end
