aca7d0bd122e0b3beab09b2b00fd7b17f14238b5
jciancio <jciancio7@gmail.com>
2016-03-17 21:56:36 -0400

    generated businesses controller and modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:new, :create, :destroy]
+  root 'welcome#index'
 
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-
   get 'signup', to: 'users#new', as: 'signup'
 
-  resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 
-  root 'welcome#index'
+  resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :businesses
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
