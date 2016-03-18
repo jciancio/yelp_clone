c47d69f5ce78a16b26bdc94ea28158931a3e7cf5
jciancio <jciancio7@gmail.com>
2016-03-17 21:39:37 -0400

    added comment/code I will want to try to implement in the future

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,8 @@ class Photo < ActiveRecord::Base
   belongs_to :business
   belongs_to :user
   belongs_to :review
+
+  # I Want to do some sort of validation that checks for
+  # either of these upon creation of a photo
+  # validates (:business || :user || :review), presence: true
 end
