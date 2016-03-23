8cedff2cdf2d7a881c30e06676cc0252797c2d26
jciancio <jciancio7@gmail.com>
2016-03-23 16:25:48 -0400

    got spec working for incorrect login, will add check for alert

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -3,12 +3,26 @@ require 'rails_helper'
 feature 'Logging In' do
   let!(:user) { create(:user) }
 
-  before do
-    sign_in(user)
+  context 'with correct user information' do
+
+    before do
+      sign_in(user)
+    end
+
+    it 'redirects to the user#show' do
+      expect(current_path).to eq(user_path(user))
+      expect(page).to have_content(@user)
+    end
   end
 
-  it 'redirects to the user#show' do
-    expect(current_path).to eq(user_path(user))
-    # expect(page).to have_content(@user)
+  context 'with incorrect user information' do
+
+    before do
+      incorrect_sign_in(user)
+    end
+
+    it 'rerenders the login page' do
+      expect(current_path).to eq(login_path)
+    end
   end
 end
