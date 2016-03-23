f60bd71911c463ed2ed8f9899578ed5157323403
jciancio <jciancio7@gmail.com>
2016-03-23 16:23:46 -0400

    added incorrect sign in helper

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -8,4 +8,14 @@ module Helpers
     fill_in 'Password', with: user.password
     click_button('Log In')
   end
+
+  def incorrect_sign_in(user)
+    visit login_path
+    find_field('Email')
+    find_field('Password')
+    find_button('Log In')
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'wrongpassword'
+    click_button('Log In')
+  end
 end
