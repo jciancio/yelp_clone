8070c194564e531f9df98398a5cdc49b9636e3d3
jciancio <jciancio7@gmail.com>
2016-03-23 19:58:47 -0400

    updated specs to take credentials, now test passing because i'm correctly testing

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  before do
-    @user = FactoryGirl.build(:user)
-    @user.email_confirmation = @user.email
-    @user.password_confirmation = @user.password
-    @user.save
-  end
+    let!(:user) { create(:user) }
 
   describe "GET #new" do
     it "returns http success" do
@@ -17,20 +12,19 @@ RSpec.describe SessionsController, type: :controller do
 
   describe "POST #create" do
     it "returns http redirect" do
-      user = @user
-      user.authenticate(@user.password)
       post :create
       expect(response).to have_http_status(:redirect)
     end
 
     it "adds user id to the session object" do
-      expect(session[:user_id]).to eq(@user.id)
+      post :create, email: user.email, password: user.password
+      expect(session[:user_id]).to eq(user.id)
     end
   end
 
   describe "DELETE #destroy" do
     it "returns http redirect" do
-      delete :destroy, id: @user.id
+      delete :destroy, id: user.id
       expect(response).to have_http_status(:redirect)
     end
 
