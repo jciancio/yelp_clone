f0eafbc0ec8bbe7f0b9f878cfac0d7faae0ecc6c
jciancio <jciancio7@gmail.com>
2016-03-22 23:52:05 -0400

    updated spec but still not running as expected

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
 feature 'Logging In' do
+  let!(:user) { create(:user) }
+
   before do
-    @user = FactoryGirl.build(:user)
-    @user.email_confirmation = @user.email
-    @user.password_confirmation = @user.password
-    @user.save
     visit login_path
     find_field('Email')
     find_field('Password')
     find_link('Log In')
-    fill_in 'Email', with: @user.email
-    fill_in 'Password', with: @user.password
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
     click_link('Log In')
   end
 
   it 'redirects to the user#show' do
-    # expect(current_path).to eq(user_path(@user))
+    expect(current_path).to eq(user_path(user))
     # expect(page).to have_content(@user)
   end
 end
