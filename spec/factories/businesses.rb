962e5121703453ad95e7540647893910c628f87c
jciancio <jciancio7@gmail.com>
2016-03-22 23:40:21 -0400

    created factory for business, hasn't been checked yet. also updated facotry for user for associations with business.

---
+++ spec/factories/businesses.rb
@@ -0,0 +1,13 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :business do
+    sequence(:name) { |n| Faker::Company.name + n.to_s }
+    description Faker::Company.catch_phrase
+    adress Faker::Address.street_address
+    city Faker::Address.city
+    state Faker::Address.state
+    zip Faker::Address.zip
+    user
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,15 @@ FactoryGirl.define do
     sequence(:name)  { |n| Faker::Name.name + n.to_s }
     password               { Faker::Internet.password(8) }
     sequence(:email)       { |n| n.to_s + Faker::Internet.free_email }
+
+    factory :user_with_businesses do
+      ignore do
+        businesses_count 5
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:business, evaluator.businesses_count, users: [user])
+      end
+    end
   end
 end
