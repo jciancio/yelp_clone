3091e9174b937b2eb151b555fe9728ba9ab91fe6
jciancio <jciancio7@gmail.com>
2016-03-23 23:39:14 -0400

    generated reviews and modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
-  resources :businesses
+  resources :businesses do
+    resources :reviews
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
