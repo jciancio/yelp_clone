e77b62372627b80feb4851d4597e23f8fb32f190
jciancio <jciancio7@gmail.com>
2016-03-23 23:34:27 -0400

    validations for both attrs

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :business
   has_one :photo, dependent: :destroy
 
+  validates :title, :description, presence: true
+
   validates :business, presence: true
 end
