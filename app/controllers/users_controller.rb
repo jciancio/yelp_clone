d674e8708b40fe1e53d35bbfe62760f1532c0ebd
jciancio <jciancio7@gmail.com>
2016-03-23 23:27:32 -0400

    check for logged in user trying to register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to @user, notice: 'User Created!'
+    # Taking account for user who tries to register when already logged in
+    if !current_user
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to @user, notice: 'User Created!'
+      else
+        render :new
+      end
     else
-      render :new
+      redirect_to user_path(current_user), notice: 'You already have an account!'
     end
   end
 
