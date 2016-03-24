3527df98bb1e0214737bfed3fbe0eb017b3f190f
jciancio <jciancio7@gmail.com>
2016-03-23 23:51:29 -0400

    generated and migrated for user reviews association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315024634) do
+ActiveRecord::Schema.define(version: 20160324035035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,9 +53,11 @@ ActiveRecord::Schema.define(version: 20160315024634) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "business_id"
+    t.integer  "user_id"
   end
 
   add_index "reviews", ["business_id"], name: "index_reviews_on_business_id", using: :btree
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -72,4 +74,5 @@ ActiveRecord::Schema.define(version: 20160315024634) do
   add_foreign_key "photos", "reviews"
   add_foreign_key "photos", "users"
   add_foreign_key "reviews", "businesses"
+  add_foreign_key "reviews", "users"
 end
