5b762828a77c0c803dc279fa7c9b656ce7fce0ff
jciancio <jciancio7@gmail.com>
2016-03-23 20:51:47 -0400

    set up the rest of users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       session[:user_id] = @user.id
       redirect_to @user, notice: 'User Created!'
@@ -22,9 +23,16 @@ class UsersController < ApplicationController
   end
 
   def update
+    if user.update(user_params)
+      redirect_to @user, notice: 'User updated!'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user.destroy
+    redirect_to root_path, notice: "Your account has been deleted."
   end
 
   private
