edde96c95a0179a88a738528fbfe5f36be74f525
jciancio <jciancio7@gmail.com>
2016-03-24 00:07:31 -0400

    stubbed out reviews controller need to test it

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,51 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_business, only: [:show, :create, :update, :destroy]
+
   def show
   end
 
   def new
+    @review = Review.new
   end
 
   def create
+    @review = current_user.reviews.build(review_params)
+    @review.business = @business
+
+    if @review.save
+      redirect_to @business, notice: 'Review created!'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @review.update(review_params)
+      redirect_to @business, notice: 'Review updated!'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @review.destroy
+    redirect_to @business, notice 'Review deleted!'
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:title, :description)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def set_business
+    @business = Business.find(params[:business_id])
   end
 end
