492b05a153fa9d366674452440d4f6f27b997126
jciancio <jciancio7@gmail.com>
2016-03-25 18:43:30 -0400

    set up js for dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require materialize-sprockets
 //= require turbolinks
+//= require materialize-sprockets
 //= require_tree .
+$(document).ready(function() {
+  $('.dropdown-button').dropdown();
+})
