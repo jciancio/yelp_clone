8791c5489bd1bbe4f757a53f0720727affe78348
jciancio <jciancio7@gmail.com>
2016-03-27 19:06:02 -0400

    implemented live search feature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,36 @@
 //= require_tree .
 $(document).ready(function() {
   $('.dropdown-button').dropdown();
+
+  $('.search').on('click', function(){
+    $('ul.right').hide();
+    $('a.left').hide();
+    $('#search-form').show();
+  });
+
+  $('#close').on('click', function() {
+    console.log('hey');
+    $('#search-form').hide();
+    $('ul.right').show();
+    $('a.left').show();
+  });
+
+  $("#search").keyup(function(){
+    // Retrieve the input field text and reset the count to zero
+    var filter = $(this).val(), count = 0;
+    // Loop through the comment list
+    $("#business-list li").each(function(){
+      // If the list item does not contain the text phrase fade it out
+      if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+        $(this).fadeOut();
+      // Show the list item if the phrase matches and increase the count by 1
+      } else {
+        $(this).show();
+        count++;
+      }
+    });
+    // Update the count
+    // var numberItems = count;
+    // $("#filter-count").text("Number of Comments = "+count);
+  });
 })
